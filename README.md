3986d5c76cc8e14ab6429e01544e0c71ae292af9
Dario Ureña <darioaxel@gmail.com>
2025-10-30 11:57:34 +0100

    Añadido el modelo Direccion y los ids como UUID

--- README.md
+++ README.md
@@ -6,20 +6,23 @@ De cada socio tendremos una serie datos personales y su dirección.
 
 ## Modelo: Socio
 
-* Identificador UUID.
-* DNI/NIE.
-* [ x ] Nombre y apellidos por separado.
-* [ x ] Fecha de nacimiento.
-* [ x ] Teléfono
-* Dirección con:
-    * calle
-    * número
-    * piso / puerta / extras (texto libre tipo "2ºB esc. izquierda")
-    * código postal
-    * ciudad (localidad)
-    * provincia
-    * país
+* [x] Identificador UUID.
+* [x] DNI/NIE.
+* [x] Nombre y apellidos por separado.
+* [x] Fecha de nacimiento.
+* [x] Teléfono
 
-* [ x ] IBAN (opcional, sólo si quiere domiciliación bancaria).
-* [ x ] Fecha de alta (auto).
+* [x] IBAN (opcional, sólo si quiere domiciliación bancaria).
+* [x] Fecha de alta (auto).
 * Info tutor si es menor: DNI tutor, nombre tutor, apellidos tutor y teléfono.
+
+## Modelo: Dirección
+Modificamos la primera versión para añadir un modelo con la dirección:
+* Dirección con:
+    * [x] calle
+    * [x] número
+    * [x] piso / puerta / extras (texto libre tipo "2ºB esc. izquierda")
+    * [x] código postal
+    * [x] ciudad (localidad)
+    * [x] provincia
+    * [x] país
--- myong_proyect/socios/models.py
+++ myong_proyect/socios/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Socio(models.Model):
+class Direccion(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    calle = models.CharField(max_length=200)
+    numero = models.CharField(max_length=10)
+    piso = models.CharField(max_length=10, blank=True, null=True)
+    otros = models.CharField(max_length=50, blank=True, null=True)
+    ciudad = models.CharField(max_length=100)
+    codigo_postal = models.DecimalField(max_digits=5, decimal_places=0)
+    pais = models.CharField(max_length=100)
     
+    def __str__(self):
+        return f"{self.calle}, {self.ciudad}, {self.pais}"
+    
+class Socio(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=200)
     fecha_nacimiento = models.DateField()
@@ -11,6 +24,7 @@ class Socio(models.Model):
     fecha_registro = models.DateTimeField(auto_now_add=True)
     menor_edad = models.BooleanField()
     IBAN = models.CharField(max_length=34, blank=True, null=True)
-   
+    documento_identidad = models.CharField(max_length=9, unique=True, null=True)
+    
     def __str__(self):
         return self.nombre
